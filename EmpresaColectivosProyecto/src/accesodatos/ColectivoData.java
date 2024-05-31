f2c36e6551db86afe10c6c1fa9c97b8e42825e6a
PabloMaidana <pablomaid1@gmail.com>
2024-05-31 18:55:56 -0300

    clases data

--- EmpresaColectivosProyecto/src/accesodatos/ColectivoData.java
+++ EmpresaColectivosProyecto/src/accesodatos/ColectivoData.java
@@ -4,10 +4,43 @@
  */
 package accesodatos;
 
+import entidades.Colectivo;
+import java.sql.*;
+
 /**
  *
  * @author pablo
  */
 public class ColectivoData {
+    private Connection con = null;
+
+    public ColectivoData() {
+        this.con = Conexion.getConexion();
+    }
     
+    public void guardarColectivo(Colectivo colectivo){
+        String sql = "INSERT INTO colectivo(matricula,marca,modelo,capacidad,estado) "
+                + "VALUES (?,?,?,?,?)";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1,colectivo.getMarca());
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al acceder a la tabla colectivo, error: " + ex);
+        }
+    }
+    
+    public void buscarColectivo(){
+        
+    }
+    
+    public void eliminarColectivo(){
+        
+    }
+    
+    public void actualizarColectivo(){
+        
+    }
 }
+
